928e1192a31bef08ebd59e99e57b0686cceb21b7
Nastya <Naska6997224@mail.ru>
2025-04-17 15:07:31 +0300

    СRUD for Categories

--- ImageHub/urls.py
+++ ImageHub/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('account.urls')),
-    path('api/', include('image.urls')),
+    path('account/', include('account.urls')),
+    path('image/', include('image.urls')),
 ]
--- image/urls.py
+++ image/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from image.views import CategoryGenericAPIView, CategoryCreateGenericAPIView, ImageGenericAPIView
+from image.views import (CategoryGenericAPIView, ImageGenericAPIView, CategoryChangeGenericAPIView,
+                         CategoryCreateGenericAPIView)
 
 urlpatterns = [
+    path('category/change/<int:pk>/', CategoryChangeGenericAPIView.as_view()),
     path('category/create/', CategoryCreateGenericAPIView.as_view()),
     path('categories/', CategoryGenericAPIView.as_view()),
     path('images/', ImageGenericAPIView.as_view()),
