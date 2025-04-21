2716b14bb8aa67a41a319a4ab6a6715057a5edb4
Nastya <Naska6997224@mail.ru>
2025-04-21 22:08:37 +0300

    docker-compose

--- ImageHub/settings.py
+++ ImageHub/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'ImageHub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str("POSTGRES_NAME"),
+        'NAME': env.str("POSTGRES_DB"),
         'USER': env.str("POSTGRES_USER"),
         'HOST': env.str("POSTGRES_HOST"),
         'PORT': env.str("POSTGRES_PORT"),
